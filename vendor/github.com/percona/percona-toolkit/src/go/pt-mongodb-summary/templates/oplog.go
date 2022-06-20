b1fb25047467c716a17f49714434cdf3980ac0df
Jason <oblitorum@gmail.com>
2022-06-20 22:10:54 +0800

    Remove vendor

--- vendor/github.com/percona/percona-toolkit/src/go/pt-mongodb-summary/templates/oplog.go
+++
@@ -1,9 +0,0 @@
-package templates
-
-const Oplog = `
-# Oplog ##################################################################################################
-Oplog Size     {{.Size}} Mb
-Oplog Used     {{.UsedMB}} Mb
-Oplog Length   {{.Running}}
-Last Election  {{.ElectionTime}}
-`
