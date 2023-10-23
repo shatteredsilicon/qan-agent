4cb838e1ae35573be54f35eb7fc99877d1996765
Jason <oblitorum@gmail.com>
2023-10-23 21:55:41 +0800

    Fix broken check for whether table prepared_statements_instances exists

--- qan/analyzer/mysql/worker/perfschema/worker.go
+++ qan/analyzer/mysql/worker/perfschema/worker.go
@@ -285,11 +285,10 @@ SELECT
 		}
 
 		preStmtDoneChan := make(chan error, 1)
-		if err = GetPreStmtRows(mysqlConn, c, preStmtDoneChan, cfg); err != nil {
-			return
+		if err = GetPreStmtRows(mysqlConn, c, preStmtDoneChan, cfg); err == nil {
+			err = <-preStmtDoneChan
 		}
 
-		err = <-preStmtDoneChan
 		if errCode, ok := err.(*mysqlDriver.MySQLError); ok && errCode.Number == 1146 {
 			// Ignore if it's a 'table not exists' error to
 			// be compatible with older mysql/mariadb versions
