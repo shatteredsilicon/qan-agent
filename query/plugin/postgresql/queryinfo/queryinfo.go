1f9d559c8bc9b42dd8558f856bd3eeb69aeed79f
Jason Ng <oblitorum@gmail.com>
2025-12-01 00:50:23 +0800

    Skip EXPLAIN for postgresql query when parameter is not resolved

--- query/plugin/postgresql/queryinfo/queryinfo.go
+++ query/plugin/postgresql/queryinfo/queryinfo.go
@@ -2,9 +2,15 @@ package queryinfo
 
 import (
 	"database/sql"
+	"encoding/json"
 
+	pg_query "github.com/pganalyze/pg_query_go/v6"
 	"github.com/shatteredsilicon/qan-agent/query/plugin/postgresql/tableinfo"
 	"github.com/shatteredsilicon/ssm/proto"
+)
+
+const (
+	MAX_OBJ_DEPTH = 100
 )
 
 // QueryInfo represents a TABLE/PROCEDURE/VIEW
@@ -20,7 +26,8 @@ type QueryInfo struct {
 // QueryInfoResult represents the response
 // of QueryInfo cmd api
 type QueryInfoResult struct {
-	Info map[string]*QueryInfo
+	Info        map[string]*QueryInfo
+	SkipExplain bool
 }
 
 func GetQueryInfo(db *sql.DB, param *proto.QueryInfoParam) (*QueryInfoResult, error) {
@@ -48,7 +55,54 @@ func GetQueryInfo(db *sql.DB, param *proto.QueryInfoParam) (*QueryInfoResult, er
 		}
 	}
 
+	var skipExplain bool
+	if len(param.QueryExample) > 0 {
+		skipExplain = shouldSkipExplain(param.QueryExample)
+	}
+
 	return &QueryInfoResult{
-		Info: res,
+		Info:        res,
+		SkipExplain: skipExplain,
 	}, nil
+}
+
+func shouldSkipExplain(query string) bool {
+	jsonStr, err := pg_query.ParseToJSON(query)
+	if err != nil {
+		return false
+	}
+
+	parseTree := map[string]interface{}{}
+	if err = json.Unmarshal([]byte(jsonStr), &parseTree); err != nil {
+		return false
+	}
+
+	return isJSONKeyExists(parseTree, "ParamRef", 0)
+}
+
+func isJSONKeyExists(data interface{}, key string, depth int) bool {
+	if depth >= MAX_OBJ_DEPTH {
+		return false
+	}
+	depth++
+
+	switch obj := data.(type) {
+	case map[string]interface{}:
+		for k, v := range obj {
+			if k == key {
+				return true
+			}
+			if exists := isJSONKeyExists(v, key, depth); exists {
+				return true
+			}
+		}
+	case []interface{}:
+		for _, v := range obj {
+			if exists := isJSONKeyExists(v, key, depth); exists {
+				return true
+			}
+		}
+	}
+
+	return false
 }
