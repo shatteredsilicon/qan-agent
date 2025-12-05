0650d9102f8d0aa713478924df4ea2df6cdb801d
Jason Ng <oblitorum@gmail.com>
2025-12-06 00:31:22 +0800

    Add procedure supports to PostgreSQL QueryInfo api

--- query/plugin/postgresql/queryinfo/queryinfo.go
+++ query/plugin/postgresql/queryinfo/queryinfo.go
@@ -9,7 +9,6 @@ import (
 	"github.com/shatteredsilicon/qan-agent/query/plugin/postgresql/tableinfo"
 	"github.com/shatteredsilicon/qan-agent/util"
 	"github.com/shatteredsilicon/ssm/proto"
-	protoQuery "github.com/shatteredsilicon/ssm/proto/query"
 )
 
 // QueryInfo represents a TABLE/PROCEDURE/VIEW
@@ -30,11 +29,17 @@ type QueryInfoResult struct {
 	SkipExplain bool
 }
 
+type ProcedureParam struct {
+	DB          string
+	Name        string
+	GuessSchema *proto.GuessDB `json:"-"`
+}
+
 type QueryInfoParam struct {
 	UUID         string
 	DB           string                 // GLOBAL database
 	Table        []tableinfo.TableParam // SHOW CREATE TABLE Db.Table
-	Procedure    []protoQuery.Procedure
+	Procedure    []ProcedureParam
 	Index        []tableinfo.TableParam // SHOW INDEXES FROM Db.Table
 	Status       []tableinfo.TableParam // SHOW TABLE STATUS FROM Db LIKE 'Table'
 	QueryExample string
@@ -114,6 +119,58 @@ func GetQueryInfo(db *sql.DB, param *QueryInfoParam) (*QueryInfoResult, error) {
 		}
 	}
 
+	if len(param.Procedure) > 0 {
+		procedureNames := make([]string, 0)
+		for i := range param.Procedure {
+			if param.Procedure[i].DB == "" {
+				procedureNames = append(procedureNames, param.Procedure[i].Name)
+			}
+		}
+
+		// there are some procedures don't have
+		// explicit schemas, we guess it
+		if len(procedureNames) > 0 {
+			guessMap, err = getGuessSchemasOfProcedures(db, procedureNames)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if len(guessMap) > 0 {
+			for i := range param.Procedure {
+				if len(param.Procedure[i].DB) > 0 {
+					continue
+				}
+				if guessSchema, ok := guessMap[param.Procedure[i].Name]; ok {
+					param.Procedure[i].DB = guessSchema.DB
+					param.Procedure[i].GuessSchema = &guessSchema
+				}
+			}
+		}
+
+		for _, p := range param.Procedure {
+			dbProcedure := p.DB + "." + p.Name
+			queryInfo, ok := res[dbProcedure]
+			if !ok {
+				res[dbProcedure] = &QueryInfo{IsSchemaGuessed: p.GuessSchema != nil && p.GuessSchema.IsAmbiguous}
+				queryInfo = res[dbProcedure]
+			}
+
+			schema := util.EscapeString(p.DB)
+			name := util.EscapeString(p.Name)
+			def, err := showCreateProcedure(db, schema, name)
+			queryInfo.Type = proto.TypeDBProcedure
+			if err != nil {
+				if queryInfo.Errors == nil {
+					queryInfo.Errors = []string{}
+				}
+				queryInfo.Errors = append(queryInfo.Errors, fmt.Sprintf("Can't get definition of procedure %s: %s", p.Name, err))
+				continue
+			}
+			queryInfo.Create = def
+		}
+	}
+
 	var skipExplain bool
 	if len(param.QueryExample) > 0 {
 		skipExplain = shouldSkipExplain(param.QueryExample)
@@ -140,8 +197,7 @@ func shouldSkipExplain(query string) bool {
 }
 
 // getGuessSchemasOfTables tries to guess the schemas of
-// tableNames (using information_schema.tables), the
-// schema with least ambiguity will be returned,
+// tableNames (using information_schema.tables),
 // a nil result will be returned if the tables are not found
 func getGuessSchemasOfTables(db *sql.DB, tableNames []string) (map[string]proto.GuessDB, error) {
 	if len(tableNames) == 0 {
@@ -191,4 +247,70 @@ func getGuessSchemasOfTables(db *sql.DB, tableNames []string) (map[string]proto.
 	}
 
 	return guessMap, nil
+}
+
+// getGuessSchemasOfProcedures tries to guess the schemas of
+// procedures (using information_schema.routines), the
+// schema with least ambiguity will be returned,
+// a nil result will be returned if the tables are not found
+func getGuessSchemasOfProcedures(db *sql.DB, procedureNames []string) (map[string]proto.GuessDB, error) {
+	if len(procedureNames) == 0 {
+		return nil, nil
+	}
+
+	names := make([]interface{}, len(procedureNames))
+	for i := range procedureNames {
+		names[i] = procedureNames[i]
+	}
+
+	// fetch 2 rows to compare, see if it's ambiguous
+	rows, err := db.Query(fmt.Sprintf(`
+		SELECT routine_schema, routine_name
+		FROM information_schema.routines
+		WHERE routine_type = 'PROCEDURE' AND routine_name IN (%s)
+	`, util.NumericPlaceholders(len(names))), names...)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	guessMap := make(map[string]proto.GuessDB)
+	for rows.Next() {
+		var schema, name string
+
+		err = rows.Scan(&schema, &name)
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if guessSchema, exists := guessMap[name]; !exists || !guessSchema.IsAmbiguous {
+			guessMap[name] = proto.GuessDB{
+				DB:          schema,
+				IsAmbiguous: exists,
+			}
+		}
+	}
+
+	return guessMap, nil
+}
+
+func showCreateProcedure(db *sql.DB, schema, name string) (string, error) {
+	var def string
+	err := db.QueryRow(`
+		SELECT pg_get_functiondef(p.oid)
+		FROM pg_proc p
+		JOIN pg_namespace n ON n.oid = p.pronamespace
+		WHERE p.proname = $1
+		AND n.nspname = $2
+	`, name, schema).Scan(&def)
+	if err == sql.ErrNoRows {
+		err = fmt.Errorf("procedure %s doesn't exist ", name)
+	}
+	return def, err
 }
