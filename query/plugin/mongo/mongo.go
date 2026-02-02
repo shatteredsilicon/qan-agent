99334e057c1aa99a9c5dfac193152ce9b37bbd53
Jason Ng <oblitorum@gmail.com>
2026-02-03 02:16:27 +0800

    Set direct connect for mongodb connection by default

--- query/plugin/mongo/mongo.go
+++ query/plugin/mongo/mongo.go
@@ -24,6 +24,7 @@ import (
 	"github.com/shatteredsilicon/qan-agent/query/plugin/mongo/explain"
 	"github.com/shatteredsilicon/qan-agent/query/plugin/mongo/summary"
 	"github.com/shatteredsilicon/ssm/proto"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
 )
 
@@ -68,7 +69,12 @@ func execExplain(cmd *proto.Cmd, in proto.Instance) (interface{}, error) {
 		return nil, err
 	}
 
-	return explain.Explain(FixDSN(in.DSN), q.Db, q.Query)
+	mongoOpts, err := MongoClientOpts(FixDSN(in.DSN))
+	if err != nil {
+		return nil, err
+	}
+
+	return explain.Explain(mongoOpts, q.Db, q.Query)
 }
 
 func execSummary(cmd *proto.Cmd, in proto.Instance) (interface{}, error) {
@@ -89,4 +95,19 @@ func FixDSN(dsn string) string {
 	}
 
 	return dsn
+}
+
+func MongoClientOpts(dsn string) (*options.ClientOptions, error) {
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	mongoOpts := options.Client().ApplyURI(dsn).SetServerAPIOptions(serverAPI)
+	if mongoOpts.Direct == nil {
+		// default to directConnection=true if it's not set
+		mongoOpts.SetDirect(true)
+	}
+
+	if err := mongoOpts.Validate(); err != nil {
+		return nil, err
+	}
+
+	return mongoOpts, nil
 }
